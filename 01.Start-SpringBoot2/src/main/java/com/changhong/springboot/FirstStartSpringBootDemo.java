02b0b0eebeb4f2ee702ab26db588689e32044517
chenliang <liang3.chen@changhong.com>
2019-09-29 14:34:21 +0800

    第一次案例提交 完整

--- 01.Start-SpringBoot2/src/main/java/com/changhong/springboot/FirstStartSpringBootDemo.java
+++ 01.Start-SpringBoot2/src/main/java/com/changhong/springboot/FirstStartSpringBootDemo.java
@@ -4,12 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
+/**
+ * 欢迎学习springboot2  后续我会给出很多关于springboot2的案例给大家学习。
+ * 有什么问题请加群 QQ ：674948698
+ */
 @SpringBootApplication
+@Slf4j
 public class FirstStartSpringBootDemo {
     public static void main(String[] args) {
-        log.info("FirstStartSpringBootDemo is starting now.");
+        log.info("FirstStartSpringBootDemo is start");
         SpringApplication.run(FirstStartSpringBootDemo.class);
-        log.info("FirstStartSpringBootDemo is starting over.");
+        log.info("FirstStartSpringBootDemo is over");
     }
 }
